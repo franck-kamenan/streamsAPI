29f56afbe1a3e5c10ef3b18ffb80128f70ac0257
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-15 23:12:54 +0200

    forEach

--- StreamDemo.java
+++ StreamDemo.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.streams;
 
+import java.util.stream.Stream;
+
 public class StreamDemo {
     public static void main(String[] args) {
         String[] strArray = "This is an array of string that is getting long".split(" ");
@@ -7,5 +9,7 @@ public class StreamDemo {
         for (String s: strArray) {
             System.out.println(s);
         }
+
+        Stream.of(strArray).forEach(element -> System.out.println(element));
     }
 }
