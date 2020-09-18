af764dfc3cf2d3f908d9ebcdc4bd56e8288d16ce
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-18 13:16:40 +0200

    Iterate

--- StreamDemo.java
+++ StreamDemo.java
@@ -1,6 +1,9 @@
 package be.intecbrussel.streams;
 
 import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class StreamDemo {
@@ -15,5 +18,12 @@ public class StreamDemo {
         stream.forEach(e -> System.out.println(e));
 
         Stream.generate(() -> new Random().nextInt(10)).limit(10).forEach(e -> System.out.println(e));
+
+        Stream<String> stringStream;
+        IntStream intStream;
+        LongStream longStream;
+        DoubleStream doubleStream;
+
+        IntStream.iterate(0, e -> e + 1).limit(10).forEach(e -> System.out.println(e));
     }
 }
