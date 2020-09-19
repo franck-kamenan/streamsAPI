098b9768277374c00aad7bddf7099541c11f3d71
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 09:45:18 +0200

    getAsInt

--- StreamDemo.java
+++ StreamDemo.java
@@ -13,14 +13,14 @@ public class StreamDemo {
 
         OptionalInt min = IntStream.range(0, 10).min();
         if(min.isPresent()){
-            System.out.println(min);
+            System.out.println(min.getAsInt());
             System.out.println("Is present.");
         } else {
             System.out.println("Is NOT present.");
         }
         System.out.println("\n");
         if(min.isEmpty()){
-            System.out.println(min);
+            System.out.println(min.getAsInt());
             System.out.println("Is empty.");
         } else {
             System.out.println("Is NOT empty.");
@@ -29,14 +29,14 @@ public class StreamDemo {
 
         OptionalInt OtherMin = IntStream.of().min();
         if(OtherMin.isPresent()){
-            System.out.println(OtherMin);
+            System.out.println(OtherMin.getAsInt());
             System.out.println("Is present.");
         } else {
             System.out.println("Is NOT present.");
         }
         System.out.println("\n");
         if(OtherMin.isEmpty()){
-            System.out.println(OtherMin);
+            // System.out.println(OtherMin.getAsInt()); //error
             System.out.println("Is empty.");
         } else {
             System.out.println("Is NOT empty.");
