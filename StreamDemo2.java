817c304b23bffeab5fc744e8659f3a468af555dc
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 12:04:33 +0200

    Map

--- StreamDemo2.java
+++ StreamDemo2.java
@@ -11,6 +11,7 @@ public class StreamDemo2 {
                  .filter(e -> e % 3 == 0)
                  .filter(e -> e > 20)
                  .limit(2)
+                 .map(e -> e * 5)
                  .forEach(e -> System.out.println(e));
     }
 }
