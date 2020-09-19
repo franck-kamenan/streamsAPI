0d3d83bd374ad226ab76272294098c400dc1812b
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 13:59:56 +0200

    mapToDouble

--- StreamDemo2.java
+++ StreamDemo2.java
@@ -12,6 +12,7 @@ public class StreamDemo2 {
                  .filter(e -> e > 20)
                  .limit(2)
                  .map(e -> e * 5)
+                 .mapToDouble(e -> e / 1.5)
                  .forEach(e -> System.out.println(e));
     }
 }
