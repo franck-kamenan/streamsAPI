262098566dc779de636ab142a36fea8a01cee058
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 14:55:55 +0200

    Map getVoornaam

--- PersonApp.java
+++ PersonApp.java
@@ -7,13 +7,14 @@ public class PersonApp {
     public static void main(String[] args) {
         Person p1 = new Person("Richard", "Night", "Male", 44, 75
                                 , 175);
-        Person p2 = new Person("Meredith", "Female", "Female", 34, 65
+        Person p2 = new Person("Meredith", "Stout", "Female", 34, 65
                 , 165);
         Person p3 = new Person("Dexter", "DeShawn", "Male", 28, 85
                 , 185);
 
         Person[] personArray = new Person[]{p1, p2, p3};
 
-        Stream.of(personArray).forEach(e -> System.out.println(e));
+        Stream.of(personArray).map(e -> e.getVoornaam())
+                              .forEach(e -> System.out.println(e));
     }
 }
