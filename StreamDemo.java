cb15404330d449b446db2730884759180d054fe7
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 10:00:49 +0200

    OptionalDouble getAsDouble

--- StreamDemo.java
+++ StreamDemo.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.streams;
 
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.Random;
 import java.util.stream.DoubleStream;
@@ -9,27 +10,9 @@ import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
-        String[] strArray = "This is an array of string that is getting long".split(" ");
-
-        OptionalInt max = IntStream.range(0, 10).max();
-        if(max.isPresent()){
-            System.out.println(max.getAsInt());
-            System.out.println("Is present.");
-        } else {
-            System.out.println("Is NOT present.");
-        }
-        System.out.println("\n");
-        if(max.isEmpty()){
-            System.out.println(max.getAsInt());
-            System.out.println("Is empty.");
-        } else {
-            System.out.println("Is NOT empty.");
-        }
-        System.out.println("\n");
-
-        OptionalInt OtherMax = IntStream.of().max();
+        OptionalDouble OtherMax = IntStream.of().average();
         if(OtherMax.isPresent()){
-            System.out.println(OtherMax.getAsInt());
+            System.out.println(OtherMax.getAsDouble());
             System.out.println("Is present.");
         } else {
             System.out.println("Is NOT present.");
@@ -37,6 +20,22 @@ public class StreamDemo {
         System.out.println("\n");
         if(OtherMax.isEmpty()){
             // System.out.println(OtherMin.getAsInt()); //error
+            System.out.println("Is empty.");
+        } else {
+            System.out.println("Is NOT empty.");
+        }
+        System.out.println("\n");
+
+        OptionalDouble rangeClosedObject = IntStream.rangeClosed(0, 10).average();
+        if(rangeClosedObject.isPresent()){
+            System.out.println(rangeClosedObject.getAsDouble());
+            System.out.println("Is present.");
+        } else {
+            System.out.println("Is NOT present.");
+        }
+        System.out.println("\n");
+        if(rangeClosedObject.isEmpty()){
+            // System.out.println(rangeClosedObject.getAsInt()); //error
             System.out.println("Is empty.");
         } else {
             System.out.println("Is NOT empty.");
