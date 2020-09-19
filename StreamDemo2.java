a4cfae58a2c74077032916a30183367b6bf6612e
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 11:42:31 +0200

    Method Chaining

--- StreamDemo2.java
+++ StreamDemo2.java
@@ -6,6 +6,8 @@ public class StreamDemo2 {
 
     public static void main(String[] args) {
 
-        IntStream.rangeClosed(0, 10).filter(e -> e % 2 == 0).forEach(e -> System.out.println(e));
+        IntStream.rangeClosed(0, 10)
+                 .filter(e -> e % 2 == 0)
+                 .forEach(e -> System.out.println(e));
     }
 }
